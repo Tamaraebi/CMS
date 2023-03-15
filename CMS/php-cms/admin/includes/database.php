aadbf2e91e3d75d6a360e0c9cb4c872f17182429
Tamara Ebi-ukuli <114423068+Tamaraebi@users.noreply.github.com>
2023-03-15 13:24:40 -0400

    Add files via upload

---
+++ CMS/php-cms/admin/includes/database.php
@@ -0,0 +1,10 @@
+<?php
+
+$connect = mysqli_connect( 
+    "sql201.epizy.com", //Host
+    "epiz_33365929", //Username
+    "HNHOyPQP7sP3B9", //Password
+    "epiz_33365929_CMS" //Database name
+);
+
+mysqli_set_charset( $connect, 'UTF8' );
