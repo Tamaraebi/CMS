f656b63e18979da51a9012d00e0144c80c721879
Tamara Ebi-ukuli <tamaraebiukuli@gmail.com>
2023-03-15 15:19:02 -0400

    added education

---
+++ CMS/php-cms/admin/education.php
@@ -0,0 +1,67 @@
+<?php
+
+include( 'includes/database.php' );
+include( 'includes/config.php' );
+include( 'includes/functions.php' );
+
+secure();
+
+if( isset( $_GET['delete'] ) )
+{
+  
+  $query = 'DELETE FROM education
+    WHERE id = '.$_GET['delete'].'
+    LIMIT 1';
+  mysqli_query( $connect, $query );
+    
+  set_message( 'Education Details has been deleted' );
+  
+  header( 'Location: education.php' );
+  die();
+  
+}
+
+include( 'includes/header.php' );
+
+$query = 'SELECT *
+  FROM educational
+  ORDER BY year DESC';
+$result = mysqli_query( $connect, $query );
+
+?>
+
+<h2>Manage Education</h2>
+
+<table>
+  <tr>
+    
+    <th align="center">ID</th>
+    <th align="center">Credential</th>
+    <th align="center">Institution</th>
+    <th align="center">Year</th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <?php while( $record = mysqli_fetch_assoc( $result ) ): ?>
+    <tr>
+      <td align="center"><?php echo $record['id']; ?></td>
+      <td align="center"><?php echo $record['credential']; ?></td>
+      <td align="center"><?php echo $record['institution']; ?></td>
+      <td align="center"><?php echo $record['year']; ?></td>
+      <td align="center"><a href="education_edit.php?id=<?php echo $record['id']; ?>">Edit</i></a></td>
+      <td align="center">
+        <a href="education.php?delete=<?php echo $record['id']; ?>" onclick="javascript:confirm('Are you sure you want to delete this education?');">Delete</i></a>
+      </td>
+    </tr>
+  <?php endwhile; ?>
+</table>
+
+<p><a href="education_add.php"><i class="fas fa-plus-square"></i> Add Education</a></p>
+
+
+<?php
+
+include( 'includes/footer.php' );
+
+?>
