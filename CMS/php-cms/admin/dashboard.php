d8a3e11bb826782c80a8f210f0c2a35b5cefb307
Tamara Ebi-ukuli <tamaraebiukuli@gmail.com>
2023-03-22 11:39:35 -0400

    updated dashboard

--- CMS/php-cms/admin/dashboard.php
+++ CMS/php-cms/admin/dashboard.php
@@ -22,6 +22,10 @@ include( 'includes/header.php' );
     </a>
   </li>
   <li>
+    <a href="skills.php">
+      Manage Skills
+    </a>
+  </li>
   <li>
      <a href="education.php">
      Manage Education
