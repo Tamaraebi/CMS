ced7cd2a73e0adb1d54c34672e4c59ce13c57103
Tamara Ebi-ukuli <tamaraebiukuli@gmail.com>
2023-04-05 13:46:01 -0400

    commit

--- admin/skills.php
+++ admin/skills.php
@@ -1,33 +1,32 @@
 <?php
 
-include('includes/database.php');
-include('includes/config.php');
-include('includes/functions.php');
+include( 'includes/database.php' );
+include( 'includes/config.php' );
+include( 'includes/functions.php' );
 
 secure();
 
-if (isset($_GET['delete'])) {
-
+if( isset( $_GET['delete'] ) )
+{
+  
   $query = 'DELETE FROM Skills
-    WHERE id = ' . $_GET['delete'] . '
+    WHERE id = '.$_GET['delete'].'
     LIMIT 1';
-  mysqli_query($connect, $query);
-
-  // If there are existing joins, they should be deleted
-  // here as well/
-
-  set_message('Skill has been deleted');
-
-  header('Location: skills.php');
+  mysqli_query( $connect, $query );
+    
+  set_message( 'skills has been deleted' );
+  
+  header( 'Location: skills.php' );
   die();
+  
 }
 
-include('includes/header.php');
+include( 'includes/header.php' );
 
 $query = 'SELECT *
   FROM Skills
-  ORDER BY name ASC';
-$result = mysqli_query($connect, $query);
+  ORDER BY title DESC';
+$result = mysqli_query( $connect, $query );
 
 ?>
 
@@ -38,40 +37,34 @@ $result = mysqli_query($connect, $query);
     <th></th>
     <th align="center">ID</th>
     <th align="left">Title</th>
-    <th align="center">URL</th>
+    <th align="left">URL</th>
     <th align="center">Percent</th>
     <th></th>
     <th></th>
     <th></th>
   </tr>
-  <?php while ($record = mysqli_fetch_assoc($result)) : ?>
+  <?php while( $record = mysqli_fetch_assoc( $result ) ): ?>
     <tr>
-      <td align="center">
-        <!-- <img src="image.php?type=project&id=<?php echo $record['id']; ?>&width=300&height=300&format=inside"> -->
-      </td>
+      
       <td align="center"><?php echo $record['id']; ?></td>
       <td align="left">
-        <?php echo htmlentities($record['name']); ?>
+        <?php echo htmlentities( $record['title'] ); ?>
       </td>
-      <td align="left">
-        <a href="<?php echo $record['url']; ?>">
-          <?php echo $record['url']; ?></a>
-      </td>
-      <td align="center"><?php echo ($record['percent']); ?>%</td>
-      <td align="center"><a href="skills_photo.php?id=<?php echo $record['id']; ?>">Photo</i></a></td>
+      <td align="center"><a href="<?php echo $record['url']; ?>"><?php echo $record['url']; ?></a></td>
+      <td align="center"><?php echo $record['percent'] ; ?>%</td>
       <td align="center"><a href="skills_edit.php?id=<?php echo $record['id']; ?>">Edit</i></a></td>
       <td align="center">
-        <a href="skills.php?delete=<?php echo $record['id']; ?>" onclick="javascript:confirm('Are you sure you want to delete this skill?');">Delete</i></a>
+        <a href="skills.php?delete=<?php echo $record['id']; ?>" onclick="javascript:confirm('Are you sure you want to delete this Skill?');">Delete</i></a>
       </td>
     </tr>
   <?php endwhile; ?>
 </table>
 
-<p><a href="skills_add.php"><i class="fas fa-plus-square"></i> Add Skill</a></p>
+<p><a href="skills_add.php"><i class="fas fa-plus-square"></i> Add skills</a></p>
 
 
 <?php
 
-include('includes/footer.php');
+include( 'includes/footer.php' );
 
 ?>
